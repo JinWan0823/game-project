6d5c128a612677037db2bfb3da1535fcfd4620a0
jinWan0823 <dhksl823@gmail.com>
2025-02-26 07:53:41 +0900

    ✨ fisher-yates-shuffle 알고리즘을 이용한 정해진 개수의 컴포넌트 무작위 배치

--- src/app/(route)/applegame/page.tsx
+++ src/app/(route)/applegame/page.tsx
@@ -1,12 +1,35 @@
 import TitleWrap from '@/_components/TitleWrap';
 
 export default function AppleGame() {
+  const numbers = [];
+  for (let i = 0; i < 170; i += 1) {
+    numbers.push((i % 9) + 1);
+  }
+  // const numbers = Array.from({ length: 170 }, (_, i) => (i % 9) + 1);
+
+  for (let i = numbers.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
   return (
     <main className="inner w-[1240px] py-[120px] mx-auto z-[99999] flex items-center justify-center">
       <section className="w-full z-[9999]">
         <TitleWrap title="APPLE GAME" />
 
-        <p>dfdf</p>
+        <div className="apple-wrap w-full">
+          <div className="game-board bg-[#fff] rounded-lg p-[12px]">
+            <ul className="grid grid-cols-[repeat(17,_minmax(0,1fr))] gap-2">
+              {numbers.map((number, idx) => (
+                <li
+                  key={idx}
+                  className="bg-gray-200 rounded flex items-center justify-center"
+                >
+                  {number}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </section>
     </main>
   );
