06ffe5d79125d28c8def939f1d361e1cb3202ccb
jinWan0823 <dhksl823@gmail.com>
2025-03-15 16:11:14 +0900

    ⚡️ 404 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from 'next';
+
+import CommonBg from '@/_components/common/CommonBg';
+import TitleWrap from '@/_components/common/TitleWrap';
+
+export const metadata: Metadata = {
+  title: '404 Page not found',
+};
+
+export default function NotFound() {
+  return (
+    <main className="inner w-[1240px] h-[100vh] py-[120px] mx-auto flex items-center justify-center">
+      <section className="text-center z-[999]">
+        <TitleWrap title="Not Found Page!" />
+        <p className="text-xl">
+          죄송합니다. 찾고 계신 페이지를 찾을 수 없습니다. <br />
+          URL을 잘못 입력하셨나요? 맞춤법을 확인하세요.
+        </p>
+
+        <button type="button" className="btn-13 mt-[20px]">
+          BACK
+        </button>
+      </section>
+      <CommonBg />
+    </main>
+  );
+}
