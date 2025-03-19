8644ba5f03f35df54f900d3f7dbbbd2e2c9f1392
jinWan0823 <dhksl823@gmail.com>
2025-03-19 19:05:50 +0900

    ⚡️ brickgame 게임종료화면 추가

--- src/_components/brickgame/BrickGameFinish.tsx
+++ src/_components/brickgame/BrickGameFinish.tsx
@@ -1,5 +1,25 @@
-export default function BrickGameFinish() {
+interface ScoreProps {
+  score: number;
+  handleGameReset: () => void;
+}
+
+export default function BrickGameFinish({
+  score,
+  handleGameReset,
+}: ScoreProps) {
   return (
-    <div className="z-[99999] absolute w-full h-full flex justify-center items-center bg-[#333333b5] top-0 left-0"></div>
+    <div className="z-[99999] absolute w-full h-full flex justify-center items-center bg-[#333333b5] top-0 left-0">
+      <div className="text-center">
+        <h2 className="text-4xl point">GAME OVER</h2>
+        <p className="text-6xl text-[#333]">{score}</p>
+        <button
+          type="button"
+          className="btn-13 mt-[20px]"
+          onClick={handleGameReset}
+        >
+          RESET !
+        </button>
+      </div>
+    </div>
   );
 }
--- src/app/(route)/brickgame/page.tsx
+++ src/app/(route)/brickgame/page.tsx
@@ -8,18 +8,40 @@ import TitleWrap from '@/_components/common/TitleWrap';
 
 export default function BrickGame() {
   const [score, setScore] = useState(0);
+  const [gameFinish, setGameFinish] = useState(false);
+  const [resetGame, SetResetGame] = useState(false);
+
+  const handleGameFinish = () => {
+    setGameFinish(true);
+    const finalScore = score;
+    const bestScore = Number(window.localStorage.getItem('Brick Game'));
+
+    if (finalScore > bestScore) {
+      window.localStorage.setItem('Brick Game', String(finalScore));
+    }
+  };
+
+  const handleGameReset = () => {
+    setGameFinish(false);
+    setScore(0);
+    SetResetGame((prev) => !prev);
+  };
 
   return (
     <main className="inner w-[1240px] py-[120px] mx-auto flex items-center justify-center">
       <section className="z-[9999]">
         <TitleWrap title="GAME LIST" />
         <p className="text-right">
-          <span>단계 : {score}</span>
-          <span className="ml-[10px]">점수 : 100</span>
+          <span className="ml-[10px]">점수 : {score}</span>
         </p>
         <div className="w-[1160px]  bg-[#dfdfdf] rounded-lg border-8 border-[--pointcolor] relative">
-          <BrickGameBoard setScore={setScore} />
-          <BrickGameFinish />
+          <BrickGameBoard
+            setScore={setScore}
+            handleGameFinish={handleGameFinish}
+          />
+          {gameFinish && (
+            <BrickGameFinish score={score} handleGameReset={handleGameReset} />
+          )}
         </div>
       </section>
       <CommonBg />
